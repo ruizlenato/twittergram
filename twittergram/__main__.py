c07bbddcd1091ac4d16a2261a2d9c98fa689c532
RuizLenato <ruizlenato@protonmail.com>
2023-07-13 21:27:45 -0300

    Chore: set httpx to log only warnings

--- twittergram/__main__.py
+++ twittergram/__main__.py
@@ -1,9 +1,9 @@
 # SPDX-License-Identifier: GPL-3.0
 # Copyright (c) 2021-2022 Luiz Renato (ruizlenato@proton.me)
-import uvloop
-import logging
 import asyncio
+import logging
 
+import uvloop
 from pyrogram import idle
 
 from .bot import Client
@@ -12,10 +12,12 @@ from .bot import Client
 log = logging.getLogger(__name__)
 logging.basicConfig(
     level=logging.INFO,
-    format="\u001B[33m%(levelname)s | \u001B[35m%(name)s \u001B[31m| %(asctime)s | \u001B[37m%(message)s",
+    format="\u001B[33m%(levelname)s | \u001B[35m%(name)s \u001B[31m| %(asctime)s | \
+\u001B[37m%(message)s",
     datefmt="%m/%d %H:%M:%S",
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
 uvloop.install()
 
