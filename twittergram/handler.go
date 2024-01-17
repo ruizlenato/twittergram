4e8198cc701e8e88027982e47ebc4001f5db1dc5
RuizLenato <ruizlenato@proton.me>
2024-01-17 00:20:05 -0300

    chore(start) consolidate the Start function, handle both the Message and the CallbackQuery

--- twittergram/handler.go
+++ twittergram/handler.go
@@ -23,6 +23,8 @@ func NewHandler(bot *telego.Bot, bh *th.BotHandler) *Handler {
 
 func (h *Handler) RegisterHandlers() {
 	h.bh.Use(database.SaveUsers)
-	h.bh.HandleMessage(modules.Start, th.CommandEqual("start"))
+	h.bh.Handle(modules.Start, th.CommandEqual("start"))
+	h.bh.Handle(modules.Start, th.CallbackDataEqual("start"))
+	h.bh.HandleCallbackQuery(modules.About, th.CallbackDataEqual("about"))
 	h.bh.HandleMessage(modules.TwitterURL, th.TextMatches(regexp.MustCompile(`https?://(?:www.|mobile.)?(?:twitter|x).com/.*?/.*?/([0-9]+)`)))
 }
--- twittergram/modules/start.go
+++ twittergram/modules/start.go
@@ -9,6 +9,30 @@ import (
 	"github.com/mymmrac/telego/telegoutil"
 )
 
+func Start(bot *telego.Bot, update telego.Update) {
+	botUser, err := bot.GetMe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if update.Message == nil {
+		bot.EditMessageText(&telego.EditMessageTextParams{
+			ChatID:      telegoutil.ID(update.CallbackQuery.Message.Chat.ID),
+			MessageID:   update.CallbackQuery.Message.MessageID,
+			Text:        fmt.Sprintf(localization.Get("start_message", *update.CallbackQuery.Message), update.CallbackQuery.Message.From.FirstName, botUser.FirstName),
+			ParseMode:   "HTML",
+			ReplyMarkup: telegoutil.InlineKeyboard(telegoutil.InlineKeyboardRow(telegoutil.InlineKeyboardButton(localization.Get("about_button", *update.CallbackQuery.Message)).WithCallbackData("about"))),
+		})
+	} else {
+		bot.SendMessage(&telego.SendMessageParams{
+			ChatID:      telegoutil.ID(update.Message.Chat.ID),
+			Text:        fmt.Sprintf(localization.Get("start_message", *update.Message), update.Message.From.FirstName, botUser.FirstName),
+			ParseMode:   "HTML",
+			ReplyMarkup: telegoutil.InlineKeyboard(telegoutil.InlineKeyboardRow(telegoutil.InlineKeyboardButton(localization.Get("about_button", *update.Message)).WithCallbackData("about"))),
+		})
+	}
+
+}
+
 func Start(bot *telego.Bot, message telego.Message) {
 	botUser, err := bot.GetMe()
 	if err != nil {
